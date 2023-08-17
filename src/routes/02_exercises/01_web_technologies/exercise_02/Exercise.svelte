0e4fcbac66dcb7a3e4b579ab725e55edb4924242
airee <aire.raidvee@ut.ee>
2023-08-17 20:30:35 +0200

    1

--- src/routes/02_exercises/01_web_technologies/exercise_02/Exercise.svelte
+++ src/routes/02_exercises/01_web_technologies/exercise_02/Exercise.svelte
@@ -1,6 +1,6 @@
- <h1>Header 1</h1>
- <h2>Header 2</h2>
- <h3>Header 3</h3>
- <h4>Header 4</h4>
- <h5>Header 5</h5>
- <h6>Header 6</h6>
+ <H1>Header 1</H1>
+ <H2>Header 2</H2>
+ <H3>Header 3</H3>
+ <H4>Header 4</H4>
+ <H5>Header 5</H5>
+ <H6>Header 6</H6>
--- src/routes/02_exercises/01_web_technologies/exercise_10/Exercise.svelte
+++ src/routes/02_exercises/01_web_technologies/exercise_10/Exercise.svelte
@@ -1,7 +1,7 @@
 <script>
   import H5 from "/home/airee/datavis-technologies-handson/src/routes/02_exercises/01_web_technologies/exercise_02/Exercise.svelte";
  
-  import A from "/home/airee/datavis-technologies-handson/src/routes/02_exercises/01_web_technologies/exercise_03/Exercise.svelte";
+  import {A} from "/home/airee/datavis-technologies-handson/src/routes/02_exercises/01_web_technologies/exercise_03/Exercise.svelte";
 
 </script>
 
