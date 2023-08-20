0121f26e7aeb035ee7341387f2f6f2ee89e4884d
airee <aire.raidvee@ut.ee>
2023-08-21 00:01:07 +0200

    22

--- src/routes/02_exercises/02_basic_visualisations/exercise_16/Exercise.svelte
+++ src/routes/02_exercises/02_basic_visualisations/exercise_16/Exercise.svelte
@@ -6,7 +6,8 @@
     const innerHeight = height - margin.top - margin.bottom;
   </script>
   
-  <svg {width} {height}>
+  <svg>
+  <svg viewBox='0 0 100 100'></svg>
     <g transform="translate({margin.left}, {margin.top})">
       <circle cx={innerWidth / 2} cy={innerHeight / 2} r="20" fill="teal" />
     </g>
