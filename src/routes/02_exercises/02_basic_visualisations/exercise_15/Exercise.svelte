58f89ad76a75d9e5e7d91fbc5aa6dc8bdc8d3cb8
airee <aire.raidvee@ut.ee>
2023-08-20 23:30:46 +0200

    1

--- src/routes/02_exercises/02_basic_visualisations/exercise_15/Exercise.svelte
+++ src/routes/02_exercises/02_basic_visualisations/exercise_15/Exercise.svelte
@@ -4,11 +4,14 @@
     const margin = { top: 5, right: 5, bottom: 5, left: 5 };
     const innerWidth = width - margin.left - margin.right;
     const innerHeight = height - margin.top - margin.bottom;
+    const cx = innerWidth / 2;
+    const cy = innerHeight / 2;
   </script>
   
   <svg {width} {height}>
     <g transform="translate({margin.left}, {margin.top})">
-      <!--  -->
+      <circle {cx} {cy} r={cx} />
+      <rect x=0 y=0 width=10 height=10 />
     </g>
   </svg>
   
@@ -17,5 +20,14 @@
       border: 1px solid black;
       border-radius: 5px;
     }
+
+    rect {
+      fill: yellow;
+    }
+
+    circle {
+      fill: skyblue;
+    }
+
   </style>
   
