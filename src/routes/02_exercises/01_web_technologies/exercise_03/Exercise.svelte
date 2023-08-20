15868347a04ddde4938376131172b0690454fa54
airee <aire.raidvee@ut.ee>
2023-08-20 18:42:02 +0200

    1

--- src/routes/02_exercises/01_web_technologies/exercise_03/Exercise.svelte
+++ src/routes/02_exercises/01_web_technologies/exercise_03/Exercise.svelte
@@ -1,7 +1,29 @@
 
 
 
+<!--
 <div A>
     The best visual channels ordered by Mackinlay's ranking: position,
 length, and angle.
 </div>
+
+
+<h1>
+	Items
+</h1>
+<List {items} />
+-->
+
+<script>
+const items = [
+		{ value: 'position'},
+		{ value: 'length' },
+        { value: 'angle' } 
+	];
+</script>
+
+
+<p>
+    The best visual channels ordered by Mackinlay's ranking: position,
+length, and angle.
+</p>
