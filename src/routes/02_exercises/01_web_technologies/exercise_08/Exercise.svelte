c458338e3f66fe2981c95718ecb68b2f41c98ccc
airee <aire.raidvee@ut.ee>
2023-08-20 19:50:47 +0200

    1

--- src/routes/02_exercises/01_web_technologies/exercise_08/Exercise.svelte
+++ src/routes/02_exercises/01_web_technologies/exercise_08/Exercise.svelte
@@ -1,5 +1,8 @@
 <script>
-  const world = 'world';
+  const nimi = 'Aire';
+  const myList = ['family', 'study', 'sauna'];
 </script> 
 
-<p>Hello {world}!</p>
+<p>
+My name is {nimi} and these are some of my favourite things:</p>
+{myList}
