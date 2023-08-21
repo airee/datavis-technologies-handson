9bed0a0ae5f44ce3e4ddcea094fbec8a3527aa35
airee <aire.raidvee@ut.ee>
2023-08-21 13:27:41 +0200

    22

--- src/routes/02_exercises/02_basic_visualisations/exercise_17/Exercise.svelte
+++ src/routes/02_exercises/02_basic_visualisations/exercise_17/Exercise.svelte
@@ -6,13 +6,24 @@
     const innerWidth = width - margin.left - margin.right;
     const innerHeight = height - margin.top - margin.bottom;
   
+    import { scaleLinear } from 'd3-scale';
+    const scale = scaleLog().domain([0, 10]);
+    
+
     // Array
     const values = [2, 4, 6, 7, 9];
   </script>
   
   <svg viewBox="0 0 {width} {height}">
     <g transform="translate({margin.left},{margin.top})">
+    <g transform="translate({values})">
+
+    <circle cx = {innerheight / 2} cy = {values[1]} r=10 />
       <!--  -->
     </g>
   </svg>
   
+
+  circle {
+      fill: skyblue;
+    }
