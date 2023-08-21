98ca275b4c29802be2ee23d91d199d48e81d2da9
airee <aire.raidvee@ut.ee>
2023-08-21 21:31:21 +0200

    1

--- src/routes/02_exercises/02_basic_visualisations/exercise_17/Exercise.svelte
+++ src/routes/02_exercises/02_basic_visualisations/exercise_17/Exercise.svelte
@@ -10,24 +10,22 @@
     import { scaleLog } from 'd3-scale';
     const scale = scaleLog().domain([0, 200]);
     
-
-    // Array
     const values = [2, 4, 6, 7, 9];
+    const logvalues = scale({values});
   </script>
 
 
-  <svg viewBox="0 0 {20} {height}">
+  <svg viewBox="0 0 {width / 12} {height}">
     <!--- <g transform="translate({margin.left},{margin.top})"> --->
-    <g transform={scale}>
-      <circle cx= {values[0] } cy={innerHeight / 2} r="2" />
+    <g transform={scale}> 
+    
+
+      <circle cx={scale({values}) } cy={innerHeight / 2} r="2" />
       <circle cx={values[1] } cy={innerHeight / 2}  r="2" />
       <circle cx={values[2] } cy={innerHeight / 2} r="2" />
       <circle cx={values[3]} cy={innerHeight / 2}  r="2" />
       <circle cx={values[4]} cy={innerHeight / 2}  r="2" />
-
-      <!--  -->
+      
+        <!--  -->
     </g>
   </svg>
-  
-
-  
